8e7eb0cf07b402371e40e489aeae64503563b5d3
kishan Sharma <71210835+kishan0921@users.noreply.github.com>
2026-04-15 16:23:42 +0530

    ert

---
+++ Suraj_Javascript_revise/Basics/chapter-1/chapter_1.js
@@ -0,0 +1,80 @@
+// For Making Notes into your code is called comments
+
+//* 1. var (old way)
+
+var title = "Let's Learn Javascript⚡";
+
+// var title = "This is one more title💫"
+
+// title = "Javascript🔥"
+// console.log(title);
+
+// *2. let ( new & modern way)
+
+let isDone = "Yes✅";
+// re-declare
+// let isDone = "No❌"
+
+// re-assign
+isDone = "No🔥";
+console.log(isDone);
+
+// *3. Const ( new & modern way)
+
+const PI = 3.14;
+// const PI = 22/7
+
+// PI = 12039120;
+console.log(PI);
+
+// Primitive Data Types ( )
+
+// *1. String
+let username = 'Suraj , "hello" '; //"SurajKumarJha"
+let para = `Hello world
+this is suraj
+what is going here
+kya haal chaal hai
+`;
+
+console.log(para);
+
+// *2. Number
+let age = 20;
+console.log(age);
+// *3. Boolean
+let isProgrammer = true;
+console.log(isProgrammer);
+
+// *4. null
+let empty = null;
+let college;
+// *5. undefined
+// ?6. symbols
+// ?7. BigInt
+
+
+// *2. Non Primitive Data types
+
+// *1. Array
+let fruits = ["🍇" , "🍊" , "🍐"]
+
+console.log(fruits)
+
+// *2. Object
+
+let Person = {
+    name:"Suraj",
+    age:20,
+    isProgrammer:true
+}
+
+console.log(Person)
+
+// *3. Function
+
+function add(){
+    console.log(10+20)
+}
+
+add()
