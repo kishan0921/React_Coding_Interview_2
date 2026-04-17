601e9ac2e7290b45e2e996295a649a8400826874
kishan Sharma <71210835+kishan0921@users.noreply.github.com>
2026-04-17 23:31:58 +0530

    5:45:00 Complete

--- React_Frontend_Projects/12MegaBlog/src/components/Select.jsx
+++ React_Frontend_Projects/12MegaBlog/src/components/Select.jsx
@@ -1,35 +1,62 @@
+// STep : 00
+// Jo piche jo conpcept use kiye hai like forwardRef hook use krna etc
+// yaha hum thoda again revision krenge.
+
+// STEP : 01
 // Note:  forwardRef ka use krenge- 2 Tarike se 
 // 1st - this file input.jsx
 // 2nd = select.jsx (export me "React.forwardRef" ye use kr liye h)
 
+//STEP : 02 
+// use "rfce" command
+
 // same jaise input.jsx me use kiya tha id - for unique id import kr liye.
 import React, {useId} from 'react'
 
+// STEP : 03
+// Simple sa yaha humara select component start hai.
 function Select({
-    // ye saare properties as object hai.
+    // // STEp 04:
+    // Selete component ke ander hum ye saare properties  daalenge as object
     // ab properties le lete hai
+    // Option dena important hai - taaki hum uss option me se select kr lenge
     options,
+    // Then, label bhi dena hoga
     label,
+    // then, class Name bhi dena hoga...and by default mostly hum  "className" empty hi dente hai (input.jsx,Button.jsx me bhi same use kiya hu)
     className = '',  // aisa mtlb className ='' empty hi hua
     // ab saare apne properties ko spread kr lete h
     ...props
 }, 
+// STEP : 05
+// then ek aur value mujhe lena hoga yaha extra "ref"
 // saath me mujhe ref bhi return krna hoga
 ref) {
 
+    // STEP : 07.1
     // use Id banate hai.
     const id = useId()
   return (
+    // STEP: 06
     // chalo ab form baane ki kosis krte hai.
-    <div className='w-full'>
-        {/* // now agar label kuch hai then && ke baad jo bhi likha h wo display krwa denge. */}
-        {label && <label 
-        // html for me id pass krne se unique id milegi hume
+     // ab yaaha pe mere pass ek div hai and className usska name "w-full" hai.
+        <div className='w-full'>
+            {/* // Now, agar kisi ne label pass kiya hai then only && ke baad jo likha hai show kro nahi to nahi.
+            // Lable component hum show krenge ni  to ni.
+            // same use kiya hu button.jsx file me */}
+            {label && <label
+            // STEP : 07
+                    // Why hum htmtFor use kiye hai ?
+            // sabse pehle Id banana hoga.  " const id = useId()", then simply hum htmlFor ke ander "id" use kr lenge.
+            // ab hume useId import kiya hu , for generating unique id
+            // so using htmlFor  and isske ander {id} pass krne se unique id milegi hume
         htmlFor={id} 
         // classNma empty hi hua
         className=''></label>}
 
+            {/* STEP 08 */}
         {/* // ab hume chahiye 1 select element  */}
+        {/* AB select ke ander aate hai options me but usse pehle properties le lete h  */}
         <select
         // ab ky select ke ander kuch properties deni hai.
         // hann , jitne bhi properties uppar hai sab pass kr do.
@@ -44,6 +71,8 @@ ref) {
         // ab thodi className bhi add kr dete h, and default value bhi de dete hai
         className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
         >
+        
+        {/*  STEP : 09 */}
         {/* // ab kyuki select use kiya hu, to options bhi dene padenge */}
 
         {/* // ab option ke ander ?. ternary operator use kiya hu. */}
@@ -59,5 +88,10 @@ ref) {
     </div>
   )
 }
+
+// STEP 10:
+// Here, do tarike se forwardRef krte hai syntax wise 
+//01- export default React.forwardRef(Select) (Select.jsx)
+//02- const Input = React.forwardRef( function Input({ (Input.jsx)
 
 export default React.forwardRef(Select)
