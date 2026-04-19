59f79dd4ce5c3086b5465228fb09648a855e784f
kishan Sharma <71210835+kishan0921@users.noreply.github.com>
2026-04-19 22:12:58 +0000

    5:18:00 completed

--- JS_Backend_code_and chai/04 Project_01 revise/src/08 index.js
+++ JS_Backend_code_and chai/04 Project_01 revise/src/08 index.js
@@ -1,65 +1,119 @@
+// STEP : 01
+// Accroding to Mongoose Documentation.
+// database hum ek line me connect kr skte hai
+// Documentation :
+// 01 - https://mongoosejs.com/
+// 02 - mongoose.connect('mongodb://127.0.0.1:27017/test');
+// But ek line waala professional approach ni hai,
+// kabhi bhi database aise ni connect krna chahiye
+
+// Hum 2 tarike se database connect krna sikhenge
+
 // require('dotenv').config({path: './env'}) // Automatically loads environment variables from .env file
 
-import dotenv from "dotenv"
-import mongoose from 'mongoose';
-import {DB_NAME} from "./constants.js";
+// Approach : 02
+// STEP : 10 (db/index.jsx firstly go to this file)
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
 
-import connectDB from './db/index.js';
+import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
-dotenv.config({ path: './.env' }); 
-
+dotenv.config({ path: "./.env" });
 
 // Jab Db connect ho jaye
 connectDB()
-
-// to then () me successful handle kr lenge
-// and catch () me error handle kr lenge
-// Yaha Then ke ander callback mil raha hai.
-.then(() =>{
+  // to then () me successful handle kr lenge
+  // and catch () me error handle kr lenge
+  // Yaha Then ke ander callback mil raha hai.
+  .then(() => {
     // App ke ander hum listen karenge, tabhi na humara server chalega
     // Then kon sa port ? so we will use process.env.PORT
-    app.listen(process.env.PORT || 8000, // Agar port nahi mila to 8000 pe chalega, jisse hum server crash hone se bacha sakte hain
- () => {
-    
+    app.listen(
+      process.env.PORT || 8000, // Agar port nahi mila to 8000 pe chalega, jisse hum server crash hone se bacha sakte hain
+      () => {
         // ${process.env.PORT} - Variable inject kar diye
         console.log(`Server is running on port ${process.env.PORT}`);
-    });
-})
+      }
+    );
+  })
 
-.catch()
+  .catch();
 
-
-
-
-
-
-
-
+// Note: 5:10:00 Ye Approach 01 accha hai, but humne thoda index.jsx code
+//zaada polluted kr liya hai. (long line of code)
+// STEP : 02
 // Approach 01: Using IIFE (Immediately Invoked Function Expression) to handle async/await
 /*  - Approach 01:
 import express from 'express';
-
 const app = express();
-
-( async () => {
-   try {
-    await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
-
-    app.on('error', (err) => {
-        console.error('Server error:', err);
-        throw err;
-    });
-
-    app.listen(process.env.PORT, () => {
-        // ${process.env.PORT} - Variable inject kar diye
-        console.log(`Server is running on port ${process.env.PORT}`);
-    });
-
-   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-    throw error;
-   }
-}) ()
-
+// Note : 
+// ( async () => { - isske uppar agar koi line likhi hui nhi hoti hai to 
+//   Hum aise likhte hai 
+// ;( async () => {
 */
+
+// Coding Start here
+// // STEP : 06
+// // Ab kai jagah aapko dikhega, ki issi same file me hum,
+// // "app" ko initialize kr lete hai.
+// // and ye app initialize hota hai using express se.
+// import express from "express";
+// const app = express();
+
+// // STEP 03:
+// // Chahiye async await le lete hai.
+// (async () => {
+//   // Now, ab database ko kaise connect kroge
+//   // wohii try-catch
+//   // Jab bhi database se connect kro, try-catch use kro hmesha
+//   try {
+//     // STEP : 05
+//     // Error part to hogayha hai.
+//     // ab agar sab kuch thik hai, Then
+//     // ye apne jo mongoose liya hai, wo aapko de dega connect()
+//     // and connect krne ke liye backticks bhi to dena hoga.
+//     // ${process.env.MONGO_URI} - ittna se database connect ho jaayega.
+//     // but itne se kaam ni hoga.
+//     // /${DB_NAME} - se hume database ka name bhi dena hota hai.
+//     // Ab ek kaam jo 100% krna hoga wo h, "await"
+//     await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+
+//     // STEP : 07
+//     // "app" initialize krne ke baad.
+//     // next line aap dekhoge listener bhi dikhte hai.
+//     // and listener hote hai, "app" ke pass
+//     // bahut saare listener hai, ussme se ek hai "error" event
+//     // "error" event ? why we are using this
+//     // Answer :- Jo skta hai ki humari app database se connect
+//     //ho chuka hai, But kya pata humari express ki app hai, wo baat ni kr
+//     // paa rahi hai.
+//     // to ye raha appne ek callback "()=>{}" laaga diya
+//     // also (err) - error yaha receive bhi to krna hoga, obviously h
+//     app.on("error", (err) => {
+//       // STEP : 08
+//       // then aapne ke message bhi pass kr diya. ki "server error hai"
+//       console.error("Server error:", err);
+//       // then, aap chaho to error throw bhi kr skte ho.
+//       // ki jab aap baat hi ni kr skte to error throw hi kr do.
+//       // then, error uppar receive bhi to krna hoga, obviously h
+//       throw err;
+//     });
+
+//     //STEP 09
+//     // Now, agar app baat kr paa raha h, then hum listen kr dete hai.
+//     // 1st - port le lo
+//     // 2nd - ek callback le lo
+//     app.listen(process.env.PORT, () => {
+//       // ${process.env.PORT} - Variable inject kar diye
+//       console.log(`Server is running on port ${process.env.PORT}`);
+//     });
+//     // STEP : 04
+//     // Catch ke ander humne apna error daal diya
+//   } catch (error) {
+//     // and aap chaho to ek message bhi send kr skte ho.
+//     console.error("Error connecting to MongoDB:", error);
+//     throw error;
+//   }
+// })();
---
+++ JS_Backend_code_and chai/04 Project_01 revise/src/db/09 index.js
@@ -0,0 +1,42 @@
+// STEP :01
+// Ab kyuki mai iss file mongoose ke through database connect krunga.
+// to mongoose import kr leta hu.
+import mongoose from "mongoose";
+
+// Then , mujhe db ka name bhi laana hoga.
+import { DB_NAME } from "./constants.js";
+
+// STEP: 02
+// Now, ab hum ek function banate hai.
+// and obviously async function hoga....kyuki db se connect krna h(db alag continent me h to wait krna hoga.)
+const connectDB = async () => {
+  // STEP : 04
+  // Db connect kar rahe to to problem aa skti hai,
+  // to try-catch block use karte hain
+  try {
+    // await to laagana hoga, kyuki db dusre continent me hai
+    // then mongoose se connect() method use kr liye
+    // then 1st variable - ${process.env.PORT} - Variable inject kar diye
+    // then 2nd variable - DB_NAME - jo ki constants.js se import kiya
+    // 'connectionInstance'variable name - ek response aa raha hai, jo ki connection instance me hum hold kar rahe h
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGO_URI}/${DB_NAME}`,
+      {}
+    );
+    // and then, print bhi rk do message "MongoDB connect hogaya h"
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+    );
+    // STEP: 03
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    // note: node js aapko access deta hai 'process ka' and aapko import bhi nahi karna padta
+    // ab process exit kr skte hai, using Exit() method
+    // process.exit(1) - 1 means failure, 0 means success
+    process.exit(1); // Exit the process with failure
+    throw error;
+  }
+};
+
+// Ab issko export karte hain, taki baaki files me use kr sake
+export default connectDB;
